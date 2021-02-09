66b4d1ec0e68bb1c7959afefcabe132e7ebe5399
Scott Wales <scott.wales@unimelb.edu.au>
2021-02-09 16:06:00 +1100

    2021-02-09 16:06:00: Run 852

--- update_landuse.py
+++ update_landuse.py
@@ -43,7 +43,7 @@ stash_landfrac_lastyear = 835
 mf = mule.DumpFile.from_file(restart)
 
 year = mf.fixed_length_header.t1_year
-year = 1650
+year = 1600
 
 print(f'Updating land use for year {year}')
 
