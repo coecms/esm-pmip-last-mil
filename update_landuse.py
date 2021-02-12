63eabb44786f207f5dbc0c424e2cff82a470ba86
Scott Wales <scott.wales@unimelb.edu.au>
2021-02-12 12:40:49 +1100

    2021-02-12 12:40:49: Run 854

--- update_landuse.py
+++ update_landuse.py
@@ -24,9 +24,10 @@ import numpy
 landuse = xarray.open_dataset('work/atmosphere/INPUT/luh2_v2h_states_cable_N96.nc').cable_fraction
 
 def normalise(da):
-    da = (numpy.clip(da, 0, 1)).round(decimals=3)
+    #da = (numpy.clip(da, 0, 1)).round(decimals=3)
     #da[0,:,:] += numpy.where(da[3:,:,:].sum('cable_type') == 0, 0.5, 0)
-    return da / da.sum('cable_type')
+    #return da / da.sum('cable_type')
+    return da
 
 class ReplaceOp(mule.DataOperator):
     def __init__(self, da):
