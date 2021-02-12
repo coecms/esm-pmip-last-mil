ff2672537b9d2b05533bb44de08e4677aed5ef78
Scott Wales <scott.wales@unimelb.edu.au>
2021-02-12 12:29:13 +1100

    2021-02-12 12:29:13: Run 854

--- update_landuse.py
+++ update_landuse.py
@@ -56,8 +56,8 @@ out = mf.copy()
 out.validate = lambda *args, **kwargs: True
 
 set_current_landuse = ReplaceOp(landuse.sel(time=year))
-set_previous_landuse = ReplaceOp(landuse.sel(time=year-1, method='nearest'))
-set_previous_landuse = set_current_landuse
+set_previous_landuse = ReplaceOp(landuse.sel(time=year+1, method='nearest'))
+#set_previous_landuse = set_current_landuse
 
 for f in mf.fields:
     if f.lbuser4 == stash_landfrac:
